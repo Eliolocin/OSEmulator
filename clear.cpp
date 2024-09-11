ac74716735d1257bba8652e1bf963807269f8893
yeunnn <adriel_donato@dlsu.edu.ph>
2024-09-11 19:11:12 +0800

    Clear Command

--- clear.cpp
+++ clear.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 void Clear() 
 {
-	std::cout << "For the clear command, once recognized, the screen clears, reprinting the header texts.\n";
+	//For the clear command, once recognized, the screen clears, reprinting the header texts
+
+	#ifdef _WIN32
+		system("CLS");  // Windows-specific command to clear the console
+	#else
+		system("clear");  // Unix-based systems' command to clear the console
+	#endif
+
+	// Reprint the header and welcome message
+	WelcomeUser("CSOPESY");
 }
